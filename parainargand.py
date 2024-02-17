077333583724a2dac8b68fa83c82184f6d3fffd7
AlexJCheng <141839887+AlexJCheng@users.noreply.github.com>
2024-02-17 14:22:28 +0800

    Update parainargand.py

--- parainargand.py
+++ parainargand.py
@@ -2,17 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Define the parameter range
-t = np.linspace(0, 30, 300)
+t = np.linspace(0, 30, 3000)
 sq = False
 gsize = 5
 a = 1
-b = 1
+b = 1 #
 c = 0
 d = 0
-e = 1
-f = 1
+e = 2 
+f = 0 #
 g = 0
-h = t
+h = 0 
 m = 1
 n = 1
 
@@ -24,8 +24,6 @@ y = e * np.sin(f * t + g)**n + h
 # y = e sin(ft + g) + h
 
 y = y * 1j # Multiply y by i
-
-
 
 # Create the plot
 plt.figure(figsize=(6,6))
@@ -39,8 +37,8 @@ plt.axvline(0, color='black', linewidth=1)  # Vertical line (x=0)
 plt.xlabel('Real')
 plt.ylabel('Imaginary')
 #plt.title("Parametric Curve on Argand Diagram")
-#plt.title(f'x = {a}cos({b}t+{c})+({d}), y = {e}cos({f}t+{g})+({h})')
-plt.title(f'x = {a}cos(t)+(0), y = {e}sin(t)+(t)')
+plt.title(f'x = cos({b}t), y = sin({f}t)')
+#plt.title(f'x = {a}cos({b}t+{c})+({d}), y = {e}sin({f}t+{g})+({h})')
 
 # Plot the start and end points
 plt.scatter(x.real[0], y.imag[0], color='green')  # Start point in green
